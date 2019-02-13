4c16499f0cb77ed53c5b70892f5eeff04f2cc7fa
James Hotblack <james.c.hotblack@gmail.com>
2019-02-13 18:05:55 +0000

    Two tests passing. Added attr_reader toGR class.

--- ruby/spec/gilded_rose_spec.rb
+++ ruby/spec/gilded_rose_spec.rb
@@ -8,6 +8,13 @@ describe GildedRose do
       GildedRose.new(items).update_quality()
       expect(items[0].name).to eq "foo"
     end
+
+    it 'adds item to Gilded Rose' do
+      items = [Item.new("foo", 0, 0)]
+      rose = GildedRose.new(items)
+      rose.update_quality()
+      expect(rose.items.length).to eq(1)
+    end
   end
 
 end
