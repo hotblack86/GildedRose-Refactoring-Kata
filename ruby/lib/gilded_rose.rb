f15b94acd84c9d636c30bd319830a6bc7ecd8e0b
James Hotblack <james.c.hotblack@gmail.com>
2019-02-13 18:27:59 +0000

    5 unit tests now for GR

--- ruby/lib/gilded_rose.rb
+++ ruby/lib/gilded_rose.rb
@@ -7,6 +7,7 @@ class GildedRose
   end
 
   def update_quality()
+    # arr = Array(@items)
     @items.each do |item|
       if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
         if item.quality > 0
--- ruby/spec/gilded_rose_spec.rb
+++ ruby/spec/gilded_rose_spec.rb
@@ -4,16 +4,37 @@ describe GildedRose do
 
   describe "#update_quality" do
     it "does not change the name" do
-      items = [Item.new("foo", 0, 0)]
+      items = [Item.new("foo", 0, 2)]
       GildedRose.new(items).update_quality()
       expect(items[0].name).to eq "foo"
     end
 
     it 'adds item to Gilded Rose' do
-      items = [Item.new("foo", 0, 0)]
+      items = [Item.new("foo", 0, 2)]
       rose = GildedRose.new(items)
       rose.update_quality()
       expect(rose.items.length).to eq(1)
+    end
+
+    it 'subtracts 1 from quality for normal item ' do
+      items = [Item.new("foo", 2, 2)]
+      rose = GildedRose.new(items)
+      rose.update_quality()
+      expect(items[0].quality).to eq(1)
+    end
+
+    it 'subtracts 2 from quality for normal item with passed sell by date' do
+      items = [Item.new("foo", 0, 4)]
+      rose = GildedRose.new(items)
+      rose.update_quality()
+      expect(items[0].quality).to eq(2)
+    end
+
+    it 'adds 1 to quality for Aged Brie' do
+      items = [Item.new("Aged Brie", 5, 4)]
+      rose = GildedRose.new(items)
+      rose.update_quality()
+      expect(items[0].quality).to eq(5)
     end
   end
 
