79ec42520bc105747e151d22b942f77d69efd50d
James Hotblack <james.c.hotblack@gmail.com>
2019-02-15 15:45:57 +0000

    16 tests covering all 4 different item types.

--- js-jasmine/spec/gilded_rose_spec.js
+++ js-jasmine/spec/gilded_rose_spec.js
@@ -7,28 +7,28 @@ describe("Normal Items", function() {
     expect(items[0].name).toEqual("foo");
   });
 
-  it("Adds normal items to shop", function() {
+  it("Adds Item to shop", function() {
     items = [ new Item("foo", 0, 2)];
     rose = new Shop(items);
     rose.updateQuality();
     expect(rose.items.length).toEqual(1);
   });
 
-  it("Subtracts 1 from quality for normal item", function() {
+  it("Quality decreases by 1", function() {
     items = [ new Item("foo", 2, 2)];
     rose = new Shop(items);
     rose.updateQuality();
     expect(items[0].quality).toEqual(1)
   });
 
-  it("Subtracts 2 from quality for normal item with passed sell by date", function() {
+  it("Quality decreases by 2 if sellIn equals 0", function() {
     items = [ new Item("foo", 0, 4)];
     rose = new Shop(items);
     rose.updateQuality();
     expect(items[0].quality).toEqual(2)
   });
 
-  it("Subtracts 1 from sellIn for normal item", function() {
+  it("SellIn decreases by 1", function() {
     items = [ new Item("foo", 2, 5)];
     rose = new Shop(items);
     rose.updateQuality();
@@ -38,28 +38,28 @@ describe("Normal Items", function() {
 
 describe("Aged Brie", function() {
 
-  it("Adds 1 to quality for Aged Brie", function() {
+  it("Quality increases by 1", function() {
     items = [ new Item("Aged Brie", 5, 4)];
     rose = new Shop(items);
     rose.updateQuality();
     expect(items[0].quality).toEqual(5)
   });
 
-  it("Aged brie will increase quality by 2 when SellIn date is 0", () => {
+  it("Quality increases by 2 when SellIn date is 0", function() {
     var item = [new Item("Aged Brie", 0, 0)]
     var gildedRose = new Shop(item);
     var items = gildedRose.updateQuality();
     expect(items[0].quality).toEqual(2)
   })
 
-  it("Aged Brie does not increase in quality when quality is already at the threshold of 50", () => {
+  it("Quality doesn't change if quality is 50", function() {
     var item = [new Item("Aged Brie", 0, 50)]
     var gildedRose = new Shop(item);
     var items = gildedRose.updateQuality();
     expect(items[0].quality).toEqual(50)
   })
 
-  it("Subtracts 1 from sellIn for Aged Brie", function() {
+  it("SellIn decreases by 1", function() {
     items = [ new Item("Aged Brie", 2, 5)];
     rose = new Shop(items);
     rose.updateQuality();
@@ -67,41 +67,60 @@ describe("Aged Brie", function() {
   });
 });
 
-describe('Tickets', () => {
+describe('Tickets', function() {
 
-  it("tickets quality goes to 0 when SellIn equals 0", () => {
+  it("Quality goes to 0 when SellIn equals 0", function() {
     var item = [new Item("tickets", 0, 100)]
     var gildedRose = new Shop(item);
     var items = gildedRose.updateQuality();
     expect(items[0].quality).toEqual(0);
   })
 
-  it('tickets quality increases by 3 if SellIn is less than 6', () => {
+  it('Quality increases by 3 if SellIn is less than 6', function() {
     var item = [new Item("tickets", 5, 30)]
     var gildedRose = new Shop(item);
     var items = gildedRose.updateQuality();
     expect(items[0].quality).toEqual(33);
   })
 
-  it('tickets quality increases by 2 if sellIn date is greater than 5', () => {
+  it('Quality increases by 2 if sellIn date is greater than 5', function() {
     var item = [new Item("tickets", 6, 30)]
     var gildedRose = new Shop(item);
     var items = gildedRose.updateQuality();
     expect(items[0].quality).toEqual(32);
   })
 
-  it("tickets quality increases by 1 if SellIn is greater or equal to 11", () => {
+  it("Quality increases by 1 if SellIn is greater or equal to 11", function() {
     var item = [new Item("tickets", 11, 30)]
     var gildedRose = new Shop(item);
     var items = gildedRose.updateQuality();
     expect(items[0].quality).toEqual(31);
   })
 
-  it("ticket quality does not depreciate if quality >= 50 AND sellIn is a positive integer ", () => {
+  it("Quality does not depreciate if quality >= 50 AND sellIn is a positive integer ", function() {
     var item = [new Item("tickets", 1, 50)]
     var gildedRose = new Shop(item);
     var items = gildedRose.updateQuality();
     expect(items[0].quality).toEqual(50);
   })
+
+  it("SellIn decreases by 1", function() {
+    items = [ new Item("tickets", 2, 5)];
+    rose = new Shop(items);
+    rose.updateQuality();
+    expect(items[0].sellIn).toEqual(1)
+  });
 })
+
+describe('Sulfuras', function() {
+  it("Never has to be sold or decreases in Quality", function() {
+    items = [ new Item("Sulfuras", 2, 5)];
+    rose = new Shop(items);
+    rose.updateQuality();
+    expect(items[0].quality).toEqual(5)
+  });
+});
+
+
+
 
