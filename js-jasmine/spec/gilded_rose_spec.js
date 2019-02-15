99981a8101de75cc80c0e69cb5f158b739699325
James Hotblack <james.c.hotblack@gmail.com>
2019-02-15 12:36:24 +0000

    5 tests now passing for GR

--- js-jasmine/spec/gilded_rose_spec.js
+++ js-jasmine/spec/gilded_rose_spec.js
@@ -7,4 +7,35 @@ describe("Gilded Rose", function() {
     expect(items[0].name).toEqual("foo");
   });
 
+  it("adds normal items to shop", function() {
+    items = [ new Item("foo", 0, 2)];
+    rose = new Shop(items);
+    rose.updateQuality();
+    expect(rose.items.length).toEqual(1);
+  });
+
+  it("subtracts 1 from quality for normal item", function() {
+    items = [ new Item("foo", 2, 2)];
+    rose = new Shop(items);
+    rose.updateQuality();
+    expect(items[0].quality).toEqual(1)
+  });
+
+  it("subtracts 2 from quality for normal item with passed sell by date", function() {
+    items = [ new Item("foo", 0, 4)];
+    rose = new Shop(items);
+    rose.updateQuality();
+    expect(items[0].quality).toEqual(2)
+  });
+
+
+  it("adds 1 to quality for Aged Brie", function() {
+    items = [ new Item("Aged Brie", 5, 4)];
+    rose = new Shop(items);
+    rose.updateQuality();
+    expect(items[0].quality).toEqual(5)
+  });
+
+
+
 });
